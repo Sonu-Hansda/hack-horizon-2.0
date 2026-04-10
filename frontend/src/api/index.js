71e11c52ba1bcb5f4504e510e8b0bad75f830a30
Sonu-Hansda <sonukumarhansda61@gmail.com>
2026-04-10 16:38:31 +0530

    add api connection with backend

---
+++ frontend/src/api/index.js
@@ -0,0 +1,6 @@
+export { default as api } from './axios';
+export { default as authAPI } from './auth';
+export { default as patientAPI } from './patient';
+
+export * from './auth';
+export * from './patient';
